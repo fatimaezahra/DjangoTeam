aa306f591cc8e52d88dea1f05656fe00395a24f0
Richard Gwin <rgwin@ymagis.com>
2018-08-02 11:39:30 +0200

    improve tests coverage

--- mastermind.py
+++ mastermind.py
@@ -1,26 +1,52 @@
 import requests
 import json
 
-base_url = 'http://172.16.37.129/'
 
-start_url = base_url + 'api/start'
-test_url = base_url + 'api/test'
+def get_variables():
+    base_url = 'http://172.16.37.129/'
+    start_url = base_url + 'api/start'
+    test_url = base_url + 'api/test'
+    TOKEN = 'tokendj'
+    size = 8
+    nb_api_calls = 0
+    return start_url, test_url, TOKEN, size, nb_api_calls
 
-TOKEN = 'tokendj'
+
+def call_test_side_effect(param):
+    return_dict = {'good': 0, 'wrong_place': 0}
+    i = 1
+    for el in param:
+        if el in '12345':
+            if i == int(el):
+                return_dict["good"] += 1
+            else:
+                return_dict["wrong_place"] += 1
+        i += 1
+    return_str = str(return_dict)
+    return_str = return_str.replace('\'', '"')
+    return return_str
 
 
-def call_start_api():
-    get_quizz_info = requests.post(start_url,
-                                   data={'token': TOKEN})
-    return get_quizz_info.text
+def call_start_api(mock=False):
+    global nb_api_calls
+    if mock:
+        return '{"size": 5}'
+    else:
+        get_quizz_info = requests.post(start_url,
+                                       data={'token': TOKEN
+                                             })
+        nb_api_calls += 1
+        return get_quizz_info.text
 
 
 def call_test_api(chain):
+    global nb_api_calls
     get_result = requests.post(test_url,
-                                   data={'token': TOKEN,
-                                        'result': chain
-                                        }
-                                   )
+                               data={'token': TOKEN,
+                                     'result': chain
+                                     }
+                               )
+    nb_api_calls += 1
     return get_result.text
 
 
@@ -28,22 +54,20 @@ def to_dict_result(result):
     return json.loads(result)
 
 
-def get_valid_numbers():
-    global size
-    valid_numbers = []
+def get_valid_numbers(size, call_test_api=call_test_api):
+    valid = []
     i = 0
-    while len(valid_numbers) < size:
+    while len(valid) < size:
         test_string = str(i) * size
         result = to_dict_result(call_test_api(test_string))
-        print(result)
         good = result['good']
-        valid_numbers.extend([i for j in range(good)])
+        valid.extend([i] * good)
         i += 1
-    return valid_numbers
+    return valid
 
 
-def get_right_position(nb, indices):
-    test_numbers = "abcde"
+def get_right_position(nb, indices, call_test_api=call_test_api, size=5):
+    test_numbers = ''.join([chr(i+97) for i in range(size)])
     for i in indices:
         new_numbers = test_numbers.replace(chr(i+97), nb)
         result = to_dict_result(call_test_api(new_numbers))
@@ -51,29 +75,24 @@ def get_right_position(nb, indices):
             return i
 
 
-#main_info = to_dict_result(call_start_api())
-#size = main_info['size']
+def get_solution(size, call_test_api=call_test_api):
+    valid_numbers = get_valid_numbers(size, call_test_api)
+    result_list = [0 for i in range(size)]
+    indices = [i for i in range(size)]
+
+    for nb in valid_numbers:
+        position = get_right_position(str(nb), indices, call_test_api, size)
+        indices.remove(position)
+        result_list[position] = nb
+    return result_list
 
 
-size = 5
+if __name__ == '__main__':
+    start_url, test_url, TOKEN, size, nb_api_calls = get_variables()
+    main_info = to_dict_result(call_start_api())
+    if 'Error' not in main_info.keys():  # quizz already started
+        size = main_info['size']
 
-'''
-valid_numbers = get_valid_numbers()
+    result = get_solution(size)
+    #call_test_api(''.join([str(el) for el in result]))
 
-result_list = [0 for i in range(size)]
-indices = [i for i in range(size)]
-
-for nb in valid_numbers:
-    position = get_right_position(str(nb), indices)
-    indices.remove(position)
-    result_list[position] = nb
-
-print(result_list)
-
-'''
-
-
-
-
-#res_start = to_dict_result(call_test_api('34567'))
-#print(res_start)
--- tests/tests_functions.py
+++ tests/tests_functions.py
@@ -1,47 +1,71 @@
 import requests
-from mastermind import get_valid_numbers, to_dict_result, call_test_api
-from mastermind import get_right_position
-
-base_url = 'http://172.16.37.129/'
-
-start_url = base_url + 'api/start'
-test_url = base_url + 'api/test'
-
-TOKEN = 'tokendj'
+from mastermind import (get_valid_numbers, to_dict_result, call_start_api,
+                        get_right_position, get_solution, get_variables,
+                        call_test_side_effect)
+from mock import Mock
 
 
-def test_call_test_api():
-    chain = '34567'
-    response = requests.post(test_url,
-                        data={'token': TOKEN,
-                              'result': chain
-                            }
-                        )
-    assert response.status_code == 200
+size = 5
+
+
+
+
+def call_start_side_effect():
+    return '{"size": 5}'
 
 
 def test_call_start_api():
-    response = requests.post(start_url,
-                             data={'token': TOKEN})
-    assert response.status_code == 200
+     response = call_start_api(True)
+     response_dict = to_dict_result(response)
+     assert 'size' in response_dict.keys() and response_dict['size'] == 5
+
+
+def test_call_test_side_effect():
+    response = call_test_side_effect('54321')
+    response_dict = to_dict_result(response)
+    assert response_dict['good'] == 1 and response_dict['wrong_place'] == 4
+
+
+def test_call_test_api():
+    response = call_test_side_effect('54321')
+    response_dict = to_dict_result(response)
+    assert response_dict['good'] == 1 and response_dict['wrong_place'] == 4
+
+
+def test_call_to_dict_result():
+    result_str = '{"good": 1, "wrong_place":3}'
+    result_dict = to_dict_result(result_str)
+    assert result_dict == {'good': 1, 'wrong_place': 3}
 
 
 def test_get_valid_numbers():
-    res = get_valid_numbers()
-    res_part_1 = ''.join([str(el) for el in res[:2]])
-    res_part_1 += 'ccc'
-    res_part_2 = 'cc'
-    res_part_2 += ''.join([str(el) for el in res[2:]])
-    test_numbers_1 = to_dict_result(call_test_api(res_part_1))
-    test_numbers_2 = to_dict_result(call_test_api(res_part_2))
-    assert  test_numbers_1['good'] + test_numbers_1['wrong_place'] + test_numbers_2['good'] + test_numbers_2['wrong_place'] == 5
+    res = get_valid_numbers(5, call_test_side_effect)
+    res_part_1 = ''.join([str(el) for el in res[:2]]) + 'ccc'
+    res_part_2 = 'cc' +  ''.join([str(el) for el in res[2:]])
+    test_numbers_1 = to_dict_result(call_test_side_effect(res_part_1))
+    test_numbers_2 = to_dict_result(call_test_side_effect(res_part_2))
+    assert test_numbers_1['good'] + test_numbers_2['good'] == 5
 
 
 def test_get_right_position():
-    valid_numbers = get_valid_numbers()
+    valid_numbers = get_valid_numbers(5, call_test_side_effect)
     nb = str(valid_numbers[0])
-    pos = get_right_position(nb, [0, 1, 2, 3, 4])
+    pos = get_right_position(nb, [0, 1, 2, 3, 4], call_test_side_effect, 5)
     l = ['c' for i in range(5)]
     l[pos] = nb
-    res_call_test = to_dict_result(call_test_api(''.join(l)))
+    res_call_test = to_dict_result(call_test_side_effect(''.join(l)))
     assert res_call_test['good'] == 1
+
+
+def test_get_solution():
+    result_list = get_solution(5, call_test_side_effect)
+    assert result_list == [1, 2, 3, 4, 5]
+
+
+def test_init_variables():
+    variables = get_variables()
+    assert variables == ('http://172.16.37.129/api/start',
+                         'http://172.16.37.129/api/test',
+                         'tokendj', 8, 0)
+
+
